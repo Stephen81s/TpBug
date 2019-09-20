8b199f94fbec4f301c7d646bffba696ab3d668a1
stephen <soupart.stephen@gmail.com>
2019-09-20 15:41:54 +0200

    premier commit

---
+++ BugManager.php
@@ -0,0 +1,44 @@
+﻿
+<?php 
+include 'Bugs.php';
+class BugsManager{
+private $bugs;
+
+
+function __construct($bugs) {
+    $this->bugs = $bugs;
+}
+
+
+function getBugs() {
+    return $this->bugs;
+}
+
+ function setBugs($bugs) {
+    $this->bugs = $bugs;
+}
+/**
+ * charcher en memoire notre source de donées
+ */
+function load(){
+    
+}
+/**
+ //ajouter un bug a la liste
+ * @param Bugs $bug
+ */ 
+function add(Bugs $bug){
+    $this->bugs[$bug->getid()]=$bug;
+}
+/**
+ * supprimer un bugs de la liste
+ * @param type $bug
+ */
+function remove(Bugs $bug){
+    if(in_array( $bug,$this->bugs)) {
+        unset($this->bugs[]);
+    }
+}
+}
+?>
+
---
+++ Bugs.php
@@ -0,0 +1,40 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+	<meta charset="utf-8" />
+</head>
+<body>
+    <?php
+    class bug{
+        public $id;
+        public $description;
+        
+        public function __construct($id, $description) {
+            $this->id = $id;
+            $this->description = $description;
+        }
+
+        
+  public function getId(){
+      return $this ->id;
+    }
+    
+    //getteurs and setteurs
+  public  function getDescription() {
+        return $this->description;
+    }
+
+  public  function setDescription($description) {
+        $this->description = $description;
+    }
+
+    
+    
+    
+  }
+    
+    ?>
+</body>
+</html>
